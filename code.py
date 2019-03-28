1180fbeb5d451317ed07683667da52251fc9d726
David Capella <d_capella@yahoo.com>
2019-03-28 17:02:11 -0500

    Create code.py
    This is the code from HackerRank.

---
+++ code.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import fileinput
+
+n = int(fileinput.input()[0])
+
+if (n % 2 != 0) or ((n % 2 == 0) and (n in range(6,21))):
+  print("Weird")
+elif (n % 2 == 0) and ((n in range(2,6)) or (n > 20)):
+  print("Not Weird")
+
+
+if __name__ == '__main__':
+  pass
+    # N = int(input())
